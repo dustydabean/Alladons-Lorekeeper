ccb77935581293e253f1781493501daaab7d87e7
itinerare <adestus@gmail.com>
2020-08-05 15:27:06 -0400

    Listing index text clarification

--- resources/views/home/trades/listings/index.blade.php
+++ resources/views/home/trades/listings/index.blade.php
@@ -14,7 +14,7 @@
 </div>
 
 <p>
-    Here are all active trade listings. Listings are active for {{ $listingDuration }} days before they expire, after which they can only be viewed via their permalink. Note that listings only display what a user is seeking or offering based on the listing as entered, and do not directly interact with the trade system or update automatically.
+    Here are all active trade listings. Listings are active for {{ $listingDuration }} days before they expire, after which they can be viewed via their permalink. Note that listings only display what a user is seeking or offering based on the listing as entered, and do not directly interact with the trade system or update automatically.
 </p>
 
 {!! $listings->render() !!}
