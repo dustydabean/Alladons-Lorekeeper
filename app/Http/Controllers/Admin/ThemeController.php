3092b4ed1109b5488b2c02a8a6819437a6894cb7
preimpression <dare020dream@gmail.com>
2021-03-28 11:41:41 -0400

    Merge branch 'extension/theme-manager' of https://github.com/preimpression/lorekeeper.private into extension/theme-manager

---
+++ app/Http/Controllers/Admin/ThemeController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Data;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+use App\Models\Theme\Theme;
+use App\Models\User\User;
+
+use App\Services\ThemeManager;
+
+use App\Http\Controllers\Controller;
+
+class ThemeController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Admin / Theme Controller
+    |--------------------------------------------------------------------------
+    |
+    | Handles creation/editing of theme categories and themes.
+    |
+    */
+
+    /**
+     * Shows the theme index.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function getThemeIndex(Request $request)
+    {
+        $query = Theme::query();
+        $data = $request->only(['name', 'sort']);
+
+        if(isset($data['sort']))
+        {
+            switch($data['sort']) {
+                case 'newest':
+                    $submissions->sortNewest();
+                    break;
+                case 'oldest':
+                    $submissions->sortOldest();
+                    break;
+                case 'name_desc':
+                    $submissions->sortAlphabetical(true);
+                    break;
+                case 'name_asc':
+                    $submissions->sortAlphabetical();
+                    break;
+            }
+        }
+
+        if(isset($data['name']))
+            $query->where('name', 'LIKE', '%'.$data['name'].'%');
+
+        return view('admin.themes.themes', [
+            'themes' => $query->paginate(20)->appends($request->query())
+        ]);
+    }
+
+    /**
+     * Shows the create theme page.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function getCreateTheme()
+    {
+        return view('admin.themes.create_edit_theme', [
+            'theme' => new Theme
+        ]);
+    }
+
+    /**
+     * Shows the edit theme page.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function getEditTheme($id)
+    {
+        $theme = Theme::find($id);
+        if(!$theme) abort(404);
+        return view('admin.themes.create_edit_theme', [
+            'theme' => $theme,
+        ]);
+    }
+
+    /**
+     * Creates or edits an theme.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Services\ThemeManager  $service
+     * @param  int|null                  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postCreateEditTheme(Request $request, ThemeManager $service, $id = null)
+    {
+        $id ? $request->validate(Theme::$updateRules) : $request->validate(Theme::$createRules);
+        $data = $request->only([
+            'name', 'creator_name', 'creator_url', 'image', 'css', 'is_active', 'is_default'
+        ]);
+        if($id && $service->updateTheme(Theme::find($id), $data, Auth::user())) {
+            flash('Theme updated successfully.')->success();
+        }
+        else if (!$id && $theme = $service->createTheme($data, Auth::user())) {
+            flash('Theme created successfully.')->success();
+            return redirect()->to('admin/data/themes/edit/'.$theme->id);
+        }
+        else {
+            foreach($service->errors()->getMessages()['error'] as $error) flash($error)->error();
+        }
+        return redirect()->back();
+    }
+
+    /**
+     * Gets the theme deletion modal.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function getDeleteTheme($id)
+    {
+        $theme = Theme::find($id);
+        return view('admin.themes._delete_theme', [
+            'theme' => $theme,
+        ]);
+    }
+
+    /**
+     * Creates or edits an theme.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  App\Services\ThemeManager  $service
+     * @param  int                       $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postDeleteTheme(Request $request, ThemeManager $service, $id)
+    {
+        if($id && $service->deleteTheme(Theme::find($id))) {
+            flash('Theme deleted successfully.')->success();
+        }
+        else {
+            foreach($service->errors()->getMessages()['error'] as $error) flash($error)->error();
+        }
+        return redirect()->to('admin/data/themes');
+    }
+
+}
---
+++ app/Services/ThemeManager.php
@@ -0,0 +1,195 @@
+<?php namespace App\Services;
+
+use App\Services\Service;
+
+use DB;
+use Config;
+
+use App\Models\Theme;
+
+class ThemeService extends Service
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Service
+    |--------------------------------------------------------------------------
+    |
+    | Handles the creation and editing of theme.
+    |
+    */
+
+    /**
+     * Creates a new theme.
+     *
+     * @param  array                  $data
+     * @param  \App\Models\User\User  $user
+     * @return bool|\App\Models\Theme\Theme
+     */
+    public function createTheme($data, $user)
+    {
+        DB::beginTransaction();
+
+        try {
+
+            $data = $this->populateData($data);
+
+            $header = null;
+            if(isset($data['image']) && $data['image']) {
+                $data['has_header'] = 1;
+                $header = $data['image'];
+                unset($data['image']);
+            }
+            else $data['has_header'] = 0;
+
+            $css = null;
+            if(isset($data['css']) && $data['css']) {
+                $data['has_css'] = 1;
+                $css = $data['css'];
+                unset($data['css']);
+            }
+            else $data['has_css'] = 0;
+
+
+            $theme = Theme::create($data);
+
+            if ($header) {
+                $this->handleImage($header, $theme->path, $theme->imageFileName, null);
+                $this->processImage($header);
+            }
+            if ($css) {
+                $this->handleImage($css, $theme->path, $theme->cssFileName, null);
+                $this->processImage($css);
+            }
+
+            $this->processImage($image);
+
+
+            return $this->commitReturn($theme);
+        } catch(\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+        return $this->rollbackReturn(false);
+    }
+
+    /**
+     * Updates an theme.
+     *
+     * @param  \App\Models\Theme\Theme  $theme
+     * @param  array                  $data
+     * @param  \App\Models\User\User  $user
+     * @return bool|\App\Models\Theme\Theme
+     */
+    public function updateTheme($theme, $data, $user)
+    {
+        DB::beginTransaction();
+
+        try {
+            if(isset($data['theme_category_id']) && $data['theme_category_id'] == 'none') $data['theme_category_id'] = null;
+
+            // More specific validation
+            if(Theme::where('name', $data['name'])->where('id', '!=', $theme->id)->exists()) throw new \Exception("The name has already been taken.");
+            if((isset($data['theme_category_id']) && $data['theme_category_id']) && !ThemeCategory::where('id', $data['theme_category_id'])->exists()) throw new \Exception("The selected theme category is invalid.");
+
+            $data = $this->populateData($data);
+
+            $header = null;
+            if(isset($data['header']) && $data['header']) {
+                $data['has_header'] = 1;
+                $header = $data['header'];
+                unset($data['header']);
+            }
+            $css = null;
+            if(isset($data['css']) && $data['css']) {
+                $data['has_css'] = 1;
+                $header = $data['css'];
+                unset($data['css']);
+            }
+
+            $theme->update($data);
+
+            $theme->update([
+                'data' => json_encode([
+                    'rarity' => isset($data['rarity']) && $data['rarity'] ? $data['rarity'] : null,
+                    'uses' => isset($data['uses']) && $data['uses'] ? $data['uses'] : null,
+                    'release' => isset($data['release']) && $data['release'] ? $data['release'] : null,
+                    'prompts' => isset($data['prompts']) && $data['prompts'] ? $data['prompts'] : null,
+                    'resell' => isset($data['currency_quantity']) ? [$data['currency_id'] => $data['currency_quantity']] : null,
+                    ])
+            ]);
+
+            if ($theme) $this->handleImage($header, $theme->headerPath, $theme->headerFileName);
+
+            return $this->commitReturn($theme);
+        } catch(\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+        return $this->rollbackReturn(false);
+    }
+
+    /**
+     * Processes user input for creating/updating an theme.
+     *
+     * @param  array                  $data
+     * @param  \App\Models\Theme\Theme  $theme
+     * @return array
+     */
+    private function populateData($data, $theme = null)
+    {
+        if(isset($data['description']) && $data['description']) $data['parsed_description'] = parse($data['description']);
+        else $data['parsed_description'] = null;
+
+        $data['hash'] = randomString(10);
+
+        if (count($data['creator_name']) != count($data['creator_url'])) throw new \Exception("Creator name to creator url count mismatch.");
+        foreach($data['creator_name'] as $creator)
+        {
+            $creators[$creator] = $data['creator_url'];
+        }
+        unset($data['creator_name']); unset($data['creator_url']);
+        $data['creators'] = json_encode($creators);
+
+        if(!isset($data['is_active'])) $data['is_active'] = 0;
+
+        if(isset($data['is_default']))
+        {
+            $oldDefault = Theme::where('is_default',1)->first();
+            if ($oldDefault) $oldDefault->update(['is_default',0]);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Deletes an theme.
+     *
+     * @param  \App\Models\Theme\Theme  $theme
+     * @return bool
+     */
+    public function deleteTheme($theme)
+    {
+        DB::beginTransaction();
+
+        try {
+            // Check first if the theme is currently owned or if some other site feature uses it
+            if(DB::table('user_themes')->where([['theme_id', '=', $theme->id], ['count', '>', 0]])->exists()) throw new \Exception("At least one user currently owns this theme. Please remove the theme(s) before deleting it.");
+            if(DB::table('character_themes')->where([['theme_id', '=', $theme->id], ['count', '>', 0]])->exists()) throw new \Exception("At least one character currently owns this theme. Please remove the theme(s) before deleting it.");
+            if(DB::table('loots')->where('rewardable_type', 'Theme')->where('rewardable_id', $theme->id)->exists()) throw new \Exception("A loot table currently distributes this theme as a potential reward. Please remove the theme before deleting it.");
+            if(DB::table('prompt_rewards')->where('rewardable_type', 'Theme')->where('rewardable_id', $theme->id)->exists()) throw new \Exception("A prompt currently distributes this theme as a reward. Please remove the theme before deleting it.");
+            if(DB::table('shop_stock')->where('theme_id', $theme->id)->exists()) throw new \Exception("A shop currently stocks this theme. Please remove the theme before deleting it.");
+
+            DB::table('themes_log')->where('theme_id', $theme->id)->delete();
+            DB::table('user_themes')->where('theme_id', $theme->id)->delete();
+            DB::table('character_themes')->where('theme_id', $theme->id)->delete();
+            $theme->tags()->delete();
+            if($theme->has_header) $this->deleteImage($theme->headerPath, $theme->headerFileName);
+            $theme->delete();
+
+            return $this->commitReturn(true);
+        } catch(\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+        return $this->rollbackReturn(false);
+    }
+
+
+}
