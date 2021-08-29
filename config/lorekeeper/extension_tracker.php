0157ab7bc12162ecce77163b32dd79a6f205a52b
itinerare <adestus@gmail.com>
2021-08-29 11:09:30 -0400

    chore: add ext tracker info

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    
+
     // FORMAT EXAMPLE.
     //
     // 'extension_tracker' => [
@@ -11,6 +11,14 @@ return [
     //     ]),
     //     'version' => '1.0.0',
     // ],
-    
+
+    'trade_listings' => [
+        'key' => 'trade_listings',
+        'wiki_key' => 'Trade_Listings',
+        'creators' => json_encode([
+            'Mercury' => 'https://github.com/itinerare/',
+        ]),
+        'version' => '1.0.0',
+    ],
 
 ];
