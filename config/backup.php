dc4550c6e2b3e3bc080f4bb20c7f8a2f518a2554
itinerare <adestus@gmail.com>
2021-10-08 09:57:07 -0400

    fix(backups): adjust config, scheduling - in counter to unexpected/undesired behavior if not an actual error

--- config/backup.php
+++ config/backup.php
@@ -190,7 +190,7 @@ return [
             'disks' => ['local'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
-                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
+                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 25000,
             ],
         ],
 
@@ -249,7 +249,7 @@ return [
              * After cleaning up the backups remove the oldest backup until
              * this amount of megabytes has been reached.
              */
-            'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
+            'delete_oldest_backups_when_using_more_megabytes_than' => 25000,
         ],
     ],
 
