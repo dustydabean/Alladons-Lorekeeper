bacb4c73f16893ce646a10afa83377f9ebff4229
itinerare <ad@itinerare.net>
2021-11-05 15:42:36 -0400

    fix(characters): breeding perm used notif url

--- config/lorekeeper/notifications.php
+++ config/lorekeeper/notifications.php
@@ -418,7 +418,7 @@ return [
     // BREEDING_PERMISSION_USED
     518 => [
         'name' => 'Breeding Permission Used',
-        'message' => '<a href="{sender_url}">{sender_name}</a> has marked a {type} breeding permission (#{permission_id}) for <a href="character/{character_slug}">{character_name}</a> as used. (<a href="{url}/used">View Breeding Permissions</a>)',
+        'message' => '<a href="{sender_url}">{sender_name}</a> has marked a {type} breeding permission (#{permission_id}) for <a href="character/{character_slug}">{character_name}</a> as used. (<a href="{url}?used=1">View Breeding Permissions</a>)',
         'url' => 'breeding-permissions'
     ],
 
