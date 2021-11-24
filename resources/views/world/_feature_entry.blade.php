f0eb54a6ab2f3d5cdaf3d54cf037b7181de83d82
itinerare <ad@itinerare.net>
2021-11-24 16:47:46 -0500

    feat(traits): world entry display for alt types - also remove alt type settings from feature creation

--- resources/views/world/_feature_entry.blade.php
+++ resources/views/world/_feature_entry.blade.php
@@ -7,11 +7,27 @@
         @if($feature->feature_category_id)
             <div><strong>Category:</strong> {!! $feature->category->displayName !!}</div>
         @endif
+        @if($feature->parent_id)
+            <div><strong>Parent Trait:</strong> {!! $feature->parent->displayName !!}</div>
+        @endif
         @if($feature->species_id)
             <div><strong>Species:</strong> {!! $feature->species->displayName !!} @if($feature->subtype_id) ({!! $feature->subtype->displayName !!} subtype) @endif</div>
         @endif
         <div class="world-entry-text parsed-text">
             {!! $feature->parsed_description !!}
         </div>
+        @if($feature->altTypes->count())
+            <h5 class="inventory-header">
+                Alternate Types
+                <a class="small collapse-toggle collapsed" href="#alt-{{ $feature->id }}" data-toggle="collapse">Show</a></h3>
+            </h5>
+            <div class="collapse show" id="alt-{{ $feature->id }}">
+                <ul>
+                    @foreach($feature->altTypes as $altType)
+                        <li>{!! $altType->displayName !!}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
 </div>
