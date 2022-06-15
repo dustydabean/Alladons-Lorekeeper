f66c142806cd32b35f48bf453f258622c8e0edad
‘AW0005’ <bunnariums@gmail.com>
2022-06-15 17:40:01 -0400

    Show timeframe on main shop page

--- app/Models/Shop/Shop.php
+++ app/Models/Shop/Shop.php
@@ -22,7 +22,7 @@ class Shop extends Model
      * @var string
      */
     protected $table = 'shops';
-    
+
     /**
      * Validation rules for creation.
      *
@@ -33,7 +33,7 @@ class Shop extends Model
         'description' => 'nullable',
         'image' => 'mimes:png',
     ];
-    
+
     /**
      * Validation rules for updating.
      *
@@ -46,7 +46,7 @@ class Shop extends Model
     ];
 
     /**********************************************************************************************
-    
+
         RELATIONS
 
     **********************************************************************************************/
@@ -54,25 +54,25 @@ class Shop extends Model
     /**
      * Get the shop stock.
      */
-    public function stock() 
+    public function stock()
     {
         return $this->hasMany('App\Models\Shop\ShopStock');
     }
-    
+
     /**
      * Get the shop stock as items for display purposes.
      */
     public function displayStock()
     {
-        return $this->belongsToMany('App\Models\Item\Item', 'shop_stock')->withPivot('item_id', 'currency_id', 'cost', 'use_user_bank', 'use_character_bank', 'is_limited_stock', 'quantity', 'purchase_limit', 'id');
+        return $this->belongsToMany('App\Models\Item\Item', 'shop_stock')->withPivot('item_id', 'currency_id', 'cost', 'use_user_bank', 'use_character_bank', 'is_limited_stock', 'quantity', 'purchase_limit', 'purchase_limit_timeframe', 'id');
     }
 
     /**********************************************************************************************
-    
+
         ACCESSORS
 
     **********************************************************************************************/
-    
+
     /**
      * Displays the shop's name, linked to its purchase page.
      *
@@ -112,7 +112,7 @@ class Shop extends Model
     {
         return public_path($this->imageDirectory);
     }
-    
+
     /**
      * Gets the URL of the model's image.
      *
