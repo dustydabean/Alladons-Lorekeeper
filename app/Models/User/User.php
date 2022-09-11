2d66496eb2a07f313fcc093c8dbf620601ffdb4a
Uri <45401276+preimpression@users.noreply.github.com>
2022-09-10 20:11:38 -0400

    hotfix: Add theme_id to user model fillable

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'alias', 'rank_id', 'email', 'password', 'is_news_unread', 'is_banned', 'has_alias', 'avatar', 'is_sales_unread'
+        'name', 'alias', 'rank_id', 'email', 'password', 'is_news_unread', 'is_banned', 'has_alias', 'avatar', 'is_sales_unread', 'theme_id',
     ];
 
     /**
