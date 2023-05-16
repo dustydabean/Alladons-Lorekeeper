f14981977a1fcff1c1fe35375b985aa9582ff317
Ne-wt <Ne-wt@users.noreply.github.com>
2023-05-16 16:49:35 +0000

    refactor: fix blade formatting

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -20,21 +20,21 @@
 
     @include('character._header', ['character' => $character])
 
-    @if($character->images()->where('is_valid', 1)->whereNotNull('transformation_id')->exists())
-    <div class="card-header mb-2">
-        <ul class="nav nav-tabs card-header-tabs">
-            @foreach ($character->images()->where('is_valid', 1)->get() as $image)
-                <li class="nav-item">
-                    <a class="nav-link form-data-button {{ $image->id == $character->image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $image->id }}">
-                        {{ $image->transformation_id ? $image->transformation->name : 'Main' }}
-                    </a>
+    @if ($character->images()->where('is_valid', 1)->whereNotNull('transformation_id')->exists())
+        <div class="card-header mb-2">
+            <ul class="nav nav-tabs card-header-tabs">
+                @foreach ($character->images()->where('is_valid', 1)->get() as $image)
+                    <li class="nav-item">
+                        <a class="nav-link form-data-button {{ $image->id == $character->image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $image->id }}">
+                            {{ $image->transformation_id ? $image->transformation->name : 'Main' }}
+                        </a>
+                    </li>
+                @endforeach
+                <li>
+                    <h3>{!! add_help('Click on a transformation to view the image. If you don\'t see the transformation you\'re looking for, it may not have been uploaded yet.') !!}</h3>
                 </li>
-            @endforeach
-            <li>
-                <h3>{!! add_help('Click on a transformation to view the image. If you don\'t see the transformation you\'re looking for, it may not have been uploaded yet.') !!}</h3>
-            </li>
-        </ul>
-    </div>
+            </ul>
+        </div>
     @endif
 
     {{-- Main Image --}}
