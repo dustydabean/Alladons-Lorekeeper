415742b96bbe61971b6b7d44b9b26e90cc11d9ea
Cylunny <102215486+Cylunny@users.noreply.github.com>
2023-08-19 10:01:28 +0200

    add ? to make sure the Imageurls work for projects that already had theme-manager installed

--- app/Models/Theme.php
+++ app/Models/Theme.php
@@ -221,7 +221,7 @@ class Theme extends Model
     public function getHeaderImageUrlAttribute()
     {
         if (!$this->has_header && !$this->themeEditor->header_image_url) return asset('images/header.png');
-        return $this->extension ? asset($this->imageDirectory . '/' . $this->headerImageFileName . '?' . $this->hash) : $this->themeEditor->header_image_url;
+        return $this->extension ? asset($this->imageDirectory . '/' . $this->headerImageFileName . '?' . $this->hash) : $this->themeEditor?->header_image_url;
     }
 
     /**
@@ -232,7 +232,7 @@ class Theme extends Model
     public function getBackgroundImageUrlAttribute()
     {
         if (!$this->has_background && !$this->themeEditor->background_image_url) return '';
-        return $this->extension_background ? asset($this->imageDirectory . '/' . $this->backgroundImageFileName . '?' . $this->hash) : $this->themeEditor->background_image_url;
+        return $this->extension_background ? asset($this->imageDirectory . '/' . $this->backgroundImageFileName . '?' . $this->hash) : $this->themeEditor?->background_image_url;
     }
 
     /**
