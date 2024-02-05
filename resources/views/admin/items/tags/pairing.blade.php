14e342c14c4e186556d8f4f142b46e8fa08c3d77
Ne-wt <Ne-wt@users.noreply.github.com>
2024-02-05 14:14:19 +0000

    refactor: fix blade formatting

--- resources/views/admin/items/tags/pairing.blade.php
+++ resources/views/admin/items/tags/pairing.blade.php
@@ -35,7 +35,7 @@
         <p>Choose a trait that this pairing item will always grant the offspring.</p>
         {!! Form::select('feature_id', $features, $tag->getData()['feature_id'] ?? null, [
             'class' => 'form-control mr-2
-                                                                feature-select',
+                                                                        feature-select',
             'placeholder' => 'Select Offspring Trait',
         ]) !!}
     </div>
@@ -45,7 +45,7 @@
         <p>Choose a species that this pairing item will grant the offspring.</p>
         {!! Form::select('species_id', $specieses, $tag->getData()['species_id'] ?? null, [
             'class' => 'form-control mr-2
-                                                                feature-select',
+                                                                        feature-select',
             'placeholder' => 'Select Offspring Species',
         ]) !!}
     </div>
@@ -55,7 +55,7 @@
         <p>Choose a subtype that this pairing item will always grant the offspring. Will not work if the species does not match the subtype.</p>
         {!! Form::select('subtype_id', $subtypes, $tag->getData()['subtype_id'] ?? null, [
             'class' => 'form-control mr-2
-                                                                feature-select',
+                                                                        feature-select',
             'placeholder' => 'Select Offspring Subtype',
         ]) !!}
     </div>
@@ -72,7 +72,7 @@
         {!! Form::label('Default Species (Optional)') !!} {!! add_help('Choose a species that should be set if both parent species are excluded.') !!}
         {!! Form::select('default_species_id', $specieses, $tag->getData()['default_species_id'] ?? null, [
             'class' => 'form-control mr-2
-                                                                feature-select',
+                                                                        feature-select',
             'placeholder' => 'Select Default Species',
         ]) !!}
     </div>
@@ -113,7 +113,7 @@
         <p>Choose a subtype that should be set if both parent subtypes are excluded.</p>
         {!! Form::select('default_subtype_ids', $subtypes, $tag->getData()['default_subtype_ids'] ?? null, [
             'class' => 'form-control mr-2
-                                                                feature-select',
+                                                                        feature-select',
             'placeholder' => 'Select Default Subtype',
         ]) !!}
     </div>
