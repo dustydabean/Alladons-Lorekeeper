f69b66bfe8b904b4a17b39fe7eded1473ae91ad6
dustydabean <ktskeet@gmail.com>
2024-03-23 12:29:46 -0700

    discord auth

--- config/lorekeeper/sites.php
+++ config/lorekeeper/sites.php
@@ -111,8 +111,8 @@ return [
         'display_name'  => 'discord',
         'regex'         => '/([A-Za-z0-9_-]+)#([0-9]+)/m',
         'icon'          => 'fab fa-discord',
-        'auth'          => 0,
-        'primary_alias' => 0,
+        'auth'          => 1,
+        'primary_alias' => 1,
     ],
 
     /**********************************************************************************************
