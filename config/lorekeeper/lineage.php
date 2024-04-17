52d0bea07d8f71fac73e57a006a68f6a96589a45
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-04-17 12:52:53 -0700

    changed lineage length

--- config/lorekeeper/lineage.php
+++ config/lorekeeper/lineage.php
@@ -11,7 +11,7 @@ return [
     // not larger numbers may begin to display oddly
     // recommended to keep at 3 or lower
     'tab_lineage_depth' => 2, // recommended to keep at 2 or lower
-    'lineage_depth' => 3,
+    'lineage_depth' => 9,
 
     // same as above, but for descendants
     // 0 = character only, descendants tab hidden
@@ -19,7 +19,7 @@ return [
     // 2 = character, children, grandchildren
     // 3 = character, children, grandchildren, great-grandchildren
     // i recommend keeping at 2 to avoid some odd display issues
-    'descendant_depth' => 2,
+    'descendant_depth' => 1,
 
     // should the lineage tab show children
     'show_children_on_tab' => true,
