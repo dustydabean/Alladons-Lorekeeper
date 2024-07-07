974b10cbf726adc48df61ffbb805a4b7e8a22ef7
Cylunny <cydraws@gmail.com>
2024-07-07 13:14:54 +0200

    fix bug that potentially allowed users to get too many rewards + add logs of such users for admins

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,8 @@ namespace App\Exceptions;
 use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Support\Facades\Log;
 
 class Handler extends ExceptionHandler
 {
@@ -53,6 +55,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ThrottleRequestsException) {
+            Log::channel('too_many_attempts')->warning('Too many attempts: ', ['user' => $request->user()->name, 'parameters' => $request->all()]);
+            flash('Too many attempts, this will be logged for the admins. Your action may have still worked as intended, please check your inventory/characters/MYOs before retrying.')->warning();
+            return redirect()->back(); 
+        }
         return parent::render($request, $exception);
     }
 }
