a02d6e6fc9f6e4c8b778811948b04e5a7c2cb165
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-07-16 21:19:47 -0700

    another error fix

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -17,7 +17,8 @@
 
     @if ($user->is_banned)
         <div class="alert alert-danger">This user has been banned.</div>
-        
+    @endif 
+
 @if(isset($user->profile->parsed_text))
     <div class="card mb-3" style="clear:both;">
         <div class="card-body">
@@ -25,43 +26,6 @@
         </div>
     </div>
 @endif
-
-<div class="card-deck mb-4 profile-assets" style="clear:both;">
-    <div class="card profile-currencies profile-assets-card">
-        <div class="card-body text-center">
-            <h5 class="card-title">Bank</h5>
-            <div class="profile-assets-content">
-                @foreach($user->getCurrencies(false) as $currency)
-                    <div>{!! $currency->display($currency->quantity) !!}</div>
-                @endforeach
-            </div>
-            <div class="text-right"><a href="{{ $user->url.'/bank' }}">View all...</a></div>
-        </div>
-    </div>
-    <div class="card profile-inventory profile-assets-card">
-        <div class="card-body text-center">
-            <h5 class="card-title">Inventory</h5>
-            <div class="profile-assets-content">
-                @if(count($items))
-                    <div class="row">
-                        @foreach($items as $item)
-                            <div class="col-md-3 col-6 profile-inventory-item">
-                                @if($item->imageUrl)
-                                    <img src="{{ $item->imageUrl }}" data-toggle="tooltip" title="{{ $item->name }}" alt="{{ $item->name }}"/>
-                                @else
-                                    <p>{{ $item->name }}</p>
-                                @endif
-                            </div>
-                        @endforeach
-                    </div>
-                @else
-                    <div>No items owned.</div>
-                @endif
-            </div>
-            <div class="text-right"><a href="{{ $user->url.'/inventory' }}">View all...</a></div>
-        </div>
-    </div>
-</div>
 
 <div class="card-deck mb-4 profile-assets" style="clear:both;">
     <div class="card profile-inventory profile-assets-card">
