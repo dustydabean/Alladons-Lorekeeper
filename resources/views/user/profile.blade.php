cc639d610e66362ad14195b852c7563700fd5ba6
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-07-16 21:32:28 -0700

    another one

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -11,28 +11,6 @@
 @section('profile-content')
     {!! breadcrumbs(['Users' => 'users', $user->name => $user->url]) !!}
 
-    @if (mb_strtolower($user->name) != mb_strtolower($name))
-        <div class="alert alert-info">This user has changed their name to <strong>{{ $user->name }}</strong>.</div>
-    @endif
-
-    @if ($user->is_banned)
-        <div class="alert alert-danger">This user has been banned.</div>
-    @endif 
-
-    @if ($user->is_deactivated)
-        <div class="alert alert-info text-center">
-            <h1>{!! $user->displayName !!}</h1>
-            <p>This account is currently deactivated, be it by staff or the user's own action. All information herein is hidden until the account is reactivated.</p>
-            @if (Auth::check() && Auth::user()->isStaff)
-                <p class="mb-0">As you are staff, you can see the profile contents below and the sidebar contents.</p>
-            @endif
-        </div>
-    @endif
-
-    @if (!$user->is_deactivated || (Auth::check() && Auth::user()->isStaff))
-        @include('user._profile_content', ['user' => $user, 'deactivated' => $user->is_deactivated])
-    @endif
-
     <div class="card-deck mb-4 profile-assets" style="clear:both;">
     <div class="card profile-inventory profile-assets-card">
     <div class="card-body text-center">
@@ -59,12 +37,26 @@
         </div>
         </div>
 
-<h2>
-    <a href="{{ $user->url.'/characters' }}">Characters</a>
-    @if(isset($sublists) && $sublists->count() > 0)
-        @foreach($sublists as $sublist)
-        / <a href="{{ $user->url.'/sublist/'.$sublist->key }}">{{ $sublist->name }}</a>
-        @endforeach
+    @if (mb_strtolower($user->name) != mb_strtolower($name))
+        <div class="alert alert-info">This user has changed their name to <strong>{{ $user->name }}</strong>.</div>
+    @endif
+
+    @if ($user->is_banned)
+        <div class="alert alert-danger">This user has been banned.</div>
+    @endif 
+
+    @if ($user->is_deactivated)
+        <div class="alert alert-info text-center">
+            <h1>{!! $user->displayName !!}</h1>
+            <p>This account is currently deactivated, be it by staff or the user's own action. All information herein is hidden until the account is reactivated.</p>
+            @if (Auth::check() && Auth::user()->isStaff)
+                <p class="mb-0">As you are staff, you can see the profile contents below and the sidebar contents.</p>
+            @endif
+        </div>
+    @endif
+
+    @if (!$user->is_deactivated || (Auth::check() && Auth::user()->isStaff))
+        @include('user._profile_content', ['user' => $user, 'deactivated' => $user->is_deactivated])
     @endif
 
 @endsection
