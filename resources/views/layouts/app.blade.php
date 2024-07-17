d6b45c94d052bf72bd8e04d88251eb101d0c961d
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-07-16 22:24:50 -0700

    timezone test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,7 +129,7 @@
     <div id="app">
 
         <div class="site-header-image" id="header" style="background-image: url('{{ $decoratorTheme?->headerImageUrl ?? $conditionalTheme?->headerImageUrl ?? $theme?->headerImageUrl ?? asset('images/header.png') }}'); position: relative;">
-            <i class="far fa-clock"></i> {!! LiveClock() !!}
+            <i class="far fa-clock"></i> {!! LiveClock("Europe/London") !!}
         </div>
 
         @include('layouts._nav')
