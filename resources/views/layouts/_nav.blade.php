9119b43889706ff1cf62ee120eb9bb466684cd6b
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-07-16 21:53:18 -0700

    add clock above nav bar

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -3,6 +3,9 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}
         </a>
+
+        <div class="miniclock justify-content-left">@include('layouts._clock')</div>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
