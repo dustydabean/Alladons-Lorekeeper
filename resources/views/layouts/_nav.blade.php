b1277eb07319da20fbb324c7a59d5419fb7391fd
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-07-31 15:12:53 -0700

    removed other clock and changed to other location

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,4 +1,4 @@
-<i class="far fa-clock"></i> {!! LiveClock() !!}
+<i class="far fa-clock"></i> {!! LiveClock("Europe/London") !!}
 <nav class="navbar navbar-expand-md navbar-dark bg-dark" id="headerNav">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
