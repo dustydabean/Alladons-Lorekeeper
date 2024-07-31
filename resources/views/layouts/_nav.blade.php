648f7d1050982d10045d0c1024c6d5c38756a4fb
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-07-31 15:19:25 -0700

    going back

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,6 +1,5 @@
-
-<nav class="navbar navbar-expand-md navbar-dark bg-dark" id="headerNav">
 <i class="far fa-clock"></i> {!! LiveClock("Europe/London") !!}
+<nav class="navbar navbar-expand-md navbar-dark bg-dark" id="headerNav">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}
