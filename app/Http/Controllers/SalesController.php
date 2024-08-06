abb9e08feb9e999a535ddc1a8acf7a927e92165d
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-08-06 15:06:03 -0700

    added theme property

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -6,6 +6,7 @@ use App\Models\Sales\Sales;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
+use App\Models\Theme;
 
 class SalesController extends Controller {
     /*
