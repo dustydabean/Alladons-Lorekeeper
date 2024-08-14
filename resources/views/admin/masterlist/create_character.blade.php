b91f5972bcfa8b3f7c71d34b44960ddf8460f28b
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-08-14 15:28:33 -0700

    aua pa

--- resources/views/admin/masterlist/create_character.blade.php
+++ resources/views/admin/masterlist/create_character.blade.php
@@ -187,14 +187,14 @@
             {!! Form::label('Adopt/PA(s)') !!}
             <div id="designerList">
                 <div class="mb-2 d-flex">
-                    {!! Form::select('designer_id[]', $userOptions, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select a Designer']) !!}
-                    {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Designer URL']) !!}
+                    {!! Form::select('designer_id[]', $userOptions, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select a PA']) !!}
+                    {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'PA URL']) !!}
                     <a href="#" class="add-designer btn btn-link" data-toggle="tooltip" title="Add another designer">+</a>
                 </div>
             </div>
             <div class="designer-row hide mb-2">
-                {!! Form::select('designer_id[]', $userOptions, null, ['class' => 'form-control mr-2 designer-select', 'placeholder' => 'Select a Designer']) !!}
-                {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Designer URL']) !!}
+                {!! Form::select('designer_id[]', $userOptions, null, ['class' => 'form-control mr-2 designer-select', 'placeholder' => 'Select a PA']) !!}
+                {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'PA URL']) !!}
                 <a href="#" class="add-designer btn btn-link" data-toggle="tooltip" title="Add another designer">+</a>
             </div>
         </div>
@@ -202,14 +202,14 @@
             {!! Form::label('AUA(s)') !!}
             <div id="artistList">
                 <div class="mb-2 d-flex">
-                    {!! Form::select('artist_id[]', $userOptions, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select an Artist']) !!}
-                    {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Artist URL']) !!}
+                    {!! Form::select('artist_id[]', $userOptions, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select an AUA']) !!}
+                    {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'AUA URL']) !!}
                     <a href="#" class="add-artist btn btn-link" data-toggle="tooltip" title="Add another artist">+</a>
                 </div>
             </div>
             <div class="artist-row hide mb-2">
-                {!! Form::select('artist_id[]', $userOptions, null, ['class' => 'form-control mr-2 artist-select', 'placeholder' => 'Select an Artist']) !!}
-                {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Artist URL']) !!}
+                {!! Form::select('artist_id[]', $userOptions, null, ['class' => 'form-control mr-2 artist-select', 'placeholder' => 'Select an AUA']) !!}
+                {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'AUA URL']) !!}
                 <a href="#" class="add-artist btn btn-link mb-2" data-toggle="tooltip" title="Add another artist">+</a>
             </div>
         </div>
