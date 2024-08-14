0ab04aa3a24ed8ce85f25b011c827c4594d1a0c1
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-08-14 15:32:46 -0700

    aua pa

--- resources/views/character/admin/_edit_credits_modal.blade.php
+++ resources/views/character/admin/_edit_credits_modal.blade.php
@@ -1,53 +1,53 @@
 {!! Form::open(['url' => 'admin/character/image/' . $image->id . '/credits']) !!}
 <div class="form-group">
-    {!! Form::label('Designer(s)') !!}
+    {!! Form::label('Adopt/PA(s)') !!}
     <div id="designerList">
         <?php $designerCount = count($image->designers); ?>
         @foreach ($image->designers as $count => $designer)
             <div class="mb-2 d-flex">
-                {!! Form::select('designer_id[' . $designer->id . ']', $users, $designer->user_id, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select a Designer']) !!}
-                {!! Form::text('designer_url[' . $designer->id . ']', $designer->url, ['class' => 'form-control mr-2', 'placeholder' => 'Designer URL']) !!}
+                {!! Form::select('designer_id[' . $designer->id . ']', $users, $designer->user_id, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select a PA']) !!}
+                {!! Form::text('designer_url[' . $designer->id . ']', $designer->url, ['class' => 'form-control mr-2', 'placeholder' => 'PA URL']) !!}
 
                 <a href="#" class="add-designer btn btn-link" data-toggle="tooltip" title="Add another designer" @if ($count != $designerCount - 1) style="visibility: hidden;" @endif>+</a>
             </div>
         @endforeach
         @if (!count($image->designers))
             <div class="mb-2 d-flex">
-                {!! Form::select('designer_id[]', $users, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select a Designer']) !!}
-                {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Designer URL']) !!}
+                {!! Form::select('designer_id[]', $users, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select a PA']) !!}
+                {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'PA URL']) !!}
 
                 <a href="#" class="add-designer btn btn-link" data-toggle="tooltip" title="Add another designer">+</a>
             </div>
         @endif
     </div>
     <div class="designer-row hide mb-2">
-        {!! Form::select('designer_id[]', $users, null, ['class' => 'form-control mr-2 designer-select', 'placeholder' => 'Select a Designer']) !!}
-        {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Designer URL']) !!}
+        {!! Form::select('designer_id[]', $users, null, ['class' => 'form-control mr-2 designer-select', 'placeholder' => 'Select a PA']) !!}
+        {!! Form::text('designer_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'PA URL']) !!}
         <a href="#" class="add-designer btn btn-link" data-toggle="tooltip" title="Add another designer">+</a>
     </div>
 </div>
 <div class="form-group">
-    {!! Form::label('Artist(s)') !!}
+    {!! Form::label('AUA(s)') !!}
     <div id="artistList">
         <?php $artistCount = count($image->artists); ?>
         @foreach ($image->artists as $count => $artist)
             <div class="mb-2 d-flex">
-                {!! Form::select('artist_id[' . $artist->id . ']', $users, $artist->user_id, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select an Artist']) !!}
-                {!! Form::text('artist_url[' . $artist->id . ']', $artist->url, ['class' => 'form-control mr-2', 'placeholder' => 'Artist URL']) !!}
+                {!! Form::select('artist_id[' . $artist->id . ']', $users, $artist->user_id, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select an AUA']) !!}
+                {!! Form::text('artist_url[' . $artist->id . ']', $artist->url, ['class' => 'form-control mr-2', 'placeholder' => 'AUA URL']) !!}
                 <a href="#" class="add-artist btn btn-link" data-toggle="tooltip" title="Add another artist" @if ($count != $artistCount - 1) style="visibility: hidden;" @endif>+</a>
             </div>
         @endforeach
         @if (!count($image->artists))
             <div class="mb-2 d-flex">
-                {!! Form::select('artist_id[]', $users, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select an Artist']) !!}
-                {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Artist URL']) !!}
+                {!! Form::select('artist_id[]', $users, null, ['class' => 'form-control mr-2 selectize', 'placeholder' => 'Select an AUA']) !!}
+                {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'AUA URL']) !!}
                 <a href="#" class="add-artist btn btn-link" data-toggle="tooltip" title="Add another artist">+</a>
             </div>
         @endif
     </div>
     <div class="artist-row hide mb-2">
-        {!! Form::select('artist_id[]', $users, null, ['class' => 'form-control mr-2 artist-select', 'placeholder' => 'Select an Artist']) !!}
-        {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Artist URL']) !!}
+        {!! Form::select('artist_id[]', $users, null, ['class' => 'form-control mr-2 artist-select', 'placeholder' => 'Select an AUA']) !!}
+        {!! Form::text('artist_url[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'AUA URL']) !!}
         <a href="#" class="add-artist btn btn-link mb-2" data-toggle="tooltip" title="Add another artist">+</a>
     </div>
 </div>
