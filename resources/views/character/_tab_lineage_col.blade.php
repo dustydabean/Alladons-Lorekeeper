5f64461405821cd0bba56dd140f56b313bec6add
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-09-04 21:45:12 -0700

    adjusting lineage thumbnail size

--- resources/views/character/_tab_lineage_col.blade.php
+++ resources/views/character/_tab_lineage_col.blade.php
@@ -5,7 +5,7 @@
         </span>
         <br>
         <a href="{{ $character ? $character->url : '#' }}" data-toggle="tooltip" data-placement="top"
-            title="{{ $character ? '<img src="' . $character->image->thumbnailUrl . '" class=\'img-thumbnail\' alt=\'Thumbnail for ' . $character->fullName . '\' / width="150" height="150">' : '<i class=\'fas fa-question-circle\'></i>' }}">
+            title="{{ $character ? '<img src="' . $character->image->thumbnailUrl . '" class=\'img-thumbnail\' alt=\'Thumbnail for ' . $character->fullName . '\' / width="80" height="80">' : '<i class=\'fas fa-question-circle\'></i>' }}">
             {!! $character ? $character->fullName : 'Unknown' !!}
         </a>
     </div>
