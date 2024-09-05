5fbca5a653058fc1a17f4b8c3eee8e24e6182a78
dustydabean <162489091+dustydabean@users.noreply.github.com>
2024-09-04 21:06:21 -0700

    no message

--- resources/views/admin/submissions/submission.blade.php
+++ resources/views/admin/submissions/submission.blade.php
@@ -232,7 +232,7 @@
                 </tr>
             </table>
         </div>
-        @include('widgets._loot_select_row', ['showLootTables' => true, 'showRaffles' => true]) <!--Removed from [] 'items' => $items, 'currencies' => $currencies,-->
+        @include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'showLootTables' => true, 'showRaffles' => true]) <!--Removed from [] -->
 
         <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
