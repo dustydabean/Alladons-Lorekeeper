01f8a255b9f0e9e12e62b045f5a1e55714acf1a1
Alliionart <Alliionart@users.noreply.github.com>
2024-12-15 06:11:41 +0000

    refactor: fix blade formatting

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -125,7 +125,7 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-            @include('layouts._searchindexbar')
+                @include('layouts._searchindexbar')
                 <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
