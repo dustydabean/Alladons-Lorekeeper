bb73c48e883a76c7bb12dd1ce78ece422e920bd3
Speedy <speedydvv@gmail.com>
2024-12-23 20:58:40 +0100

    fix(pagination): fix jump to page input (#1161)
    * fix(pagination): A ductape solution to the missing input problem
    
    * refactor: fix blade formatting
    
    * nvm, this fixes the enter problem
    
    ---------
    
    Co-authored-by: SpeedyD <SpeedyD@users.noreply.github.com>

--- resources/views/layouts/_pagination_js.blade.php
+++ resources/views/layouts/_pagination_js.blade.php
@@ -1,16 +1,23 @@
 <script>
     $('.pageSelectPopover').popover()
 
-    function onClick(e) {
+    function onPaginationClick(e) {
         const pageCurrent = new URL(window.location.href);
         pageCurrent.searchParams.set("page", e.currentTarget.parentElement.querySelector('.paginationPageRange').value);
         document.location.href = pageCurrent.href;
     }
 
     $('.pageSelectPopover').on('shown.bs.popover', function() {
-        $('.paginator-btn').on('click', onClick);
+        var $paginationPopoverClone = $('.pagination-popover-origin').clone();
+        $('.paginationPopoverContent').append($paginationPopoverClone);
+        $paginationPopoverClone.removeClass('hide pagination-popover-origin');
+        $paginationPopoverClone.addClass('pagination-popover');
+
+        $('.pageSelectPopover').popover('update')
+
+        $('.paginator-btn').on('click', onPaginationClick);
         // so you can just hit enter after moving the range bar or entering a number
-        $('.paginationPageRange').on('keypress', (e) => e.which === 13 && onClick(e));
-        $('.paginationPageText').on('keypress', (e) => e.which === 13 ? onClick(e) : true);
+        $('.paginationPageRange').on('keypress', (e) => e.which === 13 && onPaginationClick(e));
+        $('.paginationPageText').on('keypress', (e) => e.which === 13 ? onPaginationClick(e) : true);
     });
 </script>
