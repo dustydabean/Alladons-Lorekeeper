0ee219c020125bcc52f9c570d47e07b7cbe962eb
KEETPC\ktske <ktskeet@gmail.com>
2025-02-21 20:41:15 -0800

    deregged msg updated

--- resources/views/character/_image_info.blade.php
+++ resources/views/character/_image_info.blade.php
@@ -58,7 +58,7 @@
                 </div>
                 @if (!$image->character->is_myo_slot && !$image->is_valid)
                     <div class="alert alert-danger">
-                        This version of this character is outdated, and only noted here for recordkeeping purposes. Do not use as an official reference.
+                        This design is de-registered and is no longer apart of the Alladon Community. They are included in the masterlist for record purposes, heritage purposes, and for any slots this design may still have unused in the community. This design may not be reinstated as an alladon and has lost it's monetary value as an alladon design.
                     </div>
                 @endif
 
