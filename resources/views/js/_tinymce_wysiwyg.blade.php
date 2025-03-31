4a04e2fc756f674a18b4f68cfb7967f259e26302
min'ryouta kanemura <liwoyadans@gmail.com>
2025-03-31 01:28:15 -0700

    chore: updating tinymce

--- resources/views/js/_tinymce_wysiwyg.blade.php
+++ resources/views/js/_tinymce_wysiwyg.blade.php
@@ -10,13 +10,23 @@
             plugins: [
                 'advlist autolink lists link image charmap print preview anchor',
                 'searchreplace visualblocks fullscreen spoiler',
-                'insertdatetime media table paste {{ config('lorekeeper.extensions.tinymce_code_editor') ? 'codeeditor' : 'code' }} help wordcount'
+                'insertdatetime media table paste codeeditor help wordcount'
             ],
-            toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | {{ config('lorekeeper.extensions.tinymce_code_editor') ? 'codeeditor' : 'code' }}',
+            toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | codeeditor',
             content_css: [
                 '{{ asset('css/app.css') }}',
-                '{{ asset('css/lorekeeper.css') }}'
+                '{{ asset('css/lorekeeper.css') }}',
+                '{{ asset('css/custom.css') }}',
+                '{{ asset($theme?->cssUrl) }}',
+                '{{ asset($conditionalTheme?->cssUrl) }}',
+                '{{ asset($decoratorTheme?->cssUrl) }}',
+                '{{ asset('css/all.min.css') }}' //fontawesome
             ],
+            content_style: `
+            {{ str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $theme])) }}
+            {{ str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $conditionalTheme])) }}
+            {{ str_replace(['<style>', '</style>'], '', view('layouts.editable_theme', ['theme' => $decoratorTheme])) }}
+            `,
             spoiler_caption: 'Toggle Spoiler',
             target_list: false
         });
