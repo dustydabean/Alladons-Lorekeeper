b1d9a9aa3bc1436fcdb88e08231d3efab7c258d6
dustydabean <162489091+dustydabean@users.noreply.github.com>
2025-04-08 19:05:58 -0700

    Hid Update Design in character sidebar

--- resources/views/character/_sidebar.blade.php
+++ resources/views/character/_sidebar.blade.php
@@ -29,7 +29,7 @@
             <div class="sidebar-item"><a href="{{ $character->url . '/links/edit' }}" class="{{ set_active('character/' . $character->slug . '/links/edit') }}">Request Link</a></div>
             <div class="sidebar-item"><a href="{{ $character->url . '/transfer' }}" class="{{ set_active('character/' . $character->slug . '/transfer') }}">Transfer</a></div>
             @if (Auth::user()->id == $character->user_id)
-                <div class="sidebar-item"><a href="{{ $character->url . '/approval' }}" class="{{ set_active('character/' . $character->slug . '/approval') }}">Update Design</a></div>
+                <!--<div class="sidebar-item"><a href="{{ $character->url . '/approval' }}" class="{{ set_active('character/' . $character->slug . '/approval') }}">Update Design</a></div>-->
             @endif
         </li>
     @endif
