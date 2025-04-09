5019431ccda04e63c626bbada178a1a097afa7fc
dustydabean <162489091+dustydabean@users.noreply.github.com>
2025-04-08 19:15:27 -0700

    Removed Update Design

--- resources/views/character/_sidebar.blade.php
+++ resources/views/character/_sidebar.blade.php
@@ -28,9 +28,6 @@
             <div class="sidebar-item"><a href="{{ $character->url . '/profile/edit' }}" class="{{ set_active('character/' . $character->slug . '/profile/edit') }}">Edit Profile</a></div>
             <div class="sidebar-item"><a href="{{ $character->url . '/links/edit' }}" class="{{ set_active('character/' . $character->slug . '/links/edit') }}">Request Link</a></div>
             <div class="sidebar-item"><a href="{{ $character->url . '/transfer' }}" class="{{ set_active('character/' . $character->slug . '/transfer') }}">Transfer</a></div>
-            <!--@if (Auth::user()->id == $character->user_id)-->
-                <!--<div class="sidebar-item"><a href="{{ $character->url . '/approval' }}" class="{{ set_active('character/' . $character->slug . '/approval') }}">Update Design</a></div>-->
-            <!--@endif-->
         </li>
     @endif
 </ul>
