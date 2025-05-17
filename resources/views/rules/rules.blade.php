a1957410926597c5f9dc90f9d6a1d7c1dd9f4211
dustydabean <162489091+dustydabean@users.noreply.github.com>
2025-05-16 19:49:05 -0700

    Rules WIP page update

--- resources/views/rules/rules.blade.php
+++ resources/views/rules/rules.blade.php
@@ -7,11 +7,6 @@
 @section('content')
 {!! breadcrumbs(['rules' => 'rules']) !!}
 
-<p>Section 1: Species Rules
-Alladons, once purchased, are free for you to use as your own character. I’m not stone-hard set on “they must follow these rules” because dons are designed to be independent and malleable to any situation. Just remember to credit ThatAlbinoThing as the creator of the species/design. I do not condone any changes of the design of the don, only if they are commissioned customs and discussed with me prior. Small things like scars, tattoos, etc I have no quarrel with. If you want to have them do something that is completely polar opposite to what the species norm is, give it a reason to be that way. Development is important to any character! These are my only solid conditions for owning an Alladon. 
-
-If you are interested in following the lore that comes with Alladons or mingling with the rest of us, here are the conditions:
-</p>
-
+<h4>This page is WIP! Once finished there will be an announcement!</h4>
 
 @endsection
