ad413d9d76c6183e07fbc9f630ff9d15081efc6d
KEETPC\ktske <ktskeet@gmail.com>
2025-07-10 17:24:37 -0700

    Completed Species Rules

--- resources/views/rules/rules.blade.php
+++ resources/views/rules/rules.blade.php
@@ -38,12 +38,39 @@
                                             S P E C I E S &nbsp R U L E S
                                         </h5>
                                         <div class="card-body">
-                                            <p class="card-text">
-                                                RULES
+                                            <p class="card-text" style="font-family: Nunito, sans-serif;">
+                                                <h6 style="text-indent: 20px;">Alladons, once purchased, are free for you to use as your own character. 
+                                                I'm not stone-hard set on 'they must follow these rules' because dons are designed to be 
+                                                independent and malleable to any situation. Just remember to credit <u>ThatAlbinoThing</u> as the 
+                                                creator of the species/design. I do not condone changes in the design of your character once 
+                                                adopted unless discussed with me prior or commissioned by myself. Small things like scars, tattoos 
+                                                etc I have no quarrel with. If you want to have the do something that is completely polar opposite 
+                                                to what the species norm is, give it a reason to be that way. Development is important to any character! 
+                                                These are my only solid conditions of owning an alladon.</h6>
+                                                <br/>
+                                                <h6>If you're interested in following to lore that comes with alladons or mingling with the rest of us, here are the conditions:</h6><br/>
+                                                <ol><strong>
+                                                    <li>Never take an alladon's thing.</li><br/>
+                                                    <li>They are driven by instinct. They can feel the polar opposite, but the urge to act as described for the species will always be there.</li><br/>
+                                                    <li>Lower scoring alladons will be intimidated/respect higher scoring alladons naturally. </li><br/>
+                                                    <li>They must seek companionship of sorts - within the dynamic must be a leader (whether that's the alladon or not). </li><br/>
+                                                    <li>They're carnivores - vegetation will be tolerated in small doses but will eventually make the don sick. </li><br/>
+                                                    <li>D E V E L O P M E N T... Dive into the inner workings of your don, WHY do they tick the way they do? </li><br/>
+                                                    <li>Alladons are a CLOSED species, meaning you can only obtain one from purchasing from a ThatAlbinoThing account on Discord or Facebook. If you receive one via a trade/resale, please get the original owner to confirm this otherwise the resale will be invalid on my records and the alladon will be de-registered (this is to prevent character theft in the community). </li><br/>
+                                                    <li>Alladons are a 16+ species due to the nature of detail in their biology (and some of the artwork that comes with them). If I find that a minor has bought a design, I will 'lock' the design until their 16th birthday. They will also be blocked from the alladon-access-only channels on the discord. </li><br/>
+                                                    <li>If you wish to part ways with us and sell your don on, that's perfectly fine. Check Prices for how much your alladon is worth and add on all the art you'd collected for it. You are also required to message the #contact-an-admin channel on the discord, or alert an admin, that the trade has taken place and been finalised so my registration team can update the logs and introduce the new owner. Failure to do so will result in an official warning, and continued neglect of trading updates will have you blacklisted from adoption and your alladons will be deregistered. We have to be strict on this. </li><br/>
+                                                    <li>Once the owner has decided the Alladons gender, it CANNOT be changed IF they have had pouchers. OR, if a poucher has been aged up into a specific gender. </li><br/>
+                                                    <li>Your alladon is it's registry number. If you are deregistered for whatever reason then your alladons have been disowned as part of our species and will no longer be classed as an alladon. I will ask that you change the species but that is up to you. If you receive an alladon/poucher with an invalid or fake registry number, please contact me or an admin on our Discord Server so we can deal with it appropriately. </li><br/>
+                                                    <li>Selling a registered alladon to a deregistered user is currently prohibited. </li><br/>
+                                                    <li>Selling a registered alladon to a blacklisted user is prohibited and risks potentially being blacklisted (pending investigation from the admin team). </li><br/>
+                                                    <li>We are currently not honouring 'owed' gifted breed slots with dons that have been deregged. Please check our #announcements channel for our current stance and conditions with breeding slots with dons who have been deregged (If you do not want to join our discord, get in touch with a current member or an admin and we can get a screenshot for you!). </li><br/>
+                                                    <li>Currently re-registering Alladons is CLOSED! So if you purchase a de-regged design with the intention of bringing it back into the community, you will not be able to! </li><br/>
+                                                    <li>Currently, alladons can not be made into other species. This is also just a temporary measure whilst we fix up some rules (Meme creatures are fine!). </li><br/>
+                                                </ol></strong>
                                             </p>
                                         </div>
                                         <div class="card-footer">
-                                            Last Updated DATE
+                                            Last Updated <em>July 10, 2025</em>
                                         </div>
                                     </div>
                                 </div>
