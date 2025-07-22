854e30b47672b1eaf522773a571197894514e040
KEETPC\ktske <ktskeet@gmail.com>
2025-07-21 20:22:17 -0700

    Added temp guides page to nav bar

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -58,6 +58,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('gallery') }}"> Gallery</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('info/guides') }}"> Guides</a>
+                </li>
 
                 <li class="nav-item dropdown">
                     <a id="loreDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
