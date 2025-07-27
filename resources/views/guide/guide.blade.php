c6500891c7acab21e08c622e2d64f8124993b27e
KEETPC\ktske <ktskeet@gmail.com>
2025-07-27 15:13:20 -0700

    guide page test

--- resources/views/guide/guide.blade.php
+++ resources/views/guide/guide.blade.php
@@ -9,7 +9,7 @@
 
 <div class="panel panel-default">
   <div class="panel-heading">Panel Heading</div>
-  <div class="panel-body">Panel Content</div>
+  <div class="panel-body">Panel Content Testing</div>
 </div>
 
 @endsection
