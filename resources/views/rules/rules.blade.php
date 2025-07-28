eba378b0c3fc8f24ff315e91a9b6d26fff7a124e
KEETPC\ktske <ktskeet@gmail.com>
2025-07-28 10:56:42 -0700

    removed WIP title from rules

--- resources/views/rules/rules.blade.php
+++ resources/views/rules/rules.blade.php
@@ -7,9 +7,6 @@
 @section('content')
 {!! breadcrumbs(['rules' => 'rules']) !!}
 
-<div class="card-mut">This page is WIP! Once finished there will be an announcement!</div>
-
-<br>
 <br>
 
 <div class="container-fluid">
