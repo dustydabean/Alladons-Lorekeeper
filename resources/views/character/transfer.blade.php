1945559285b2a8c592629ddf108acde62716efc0
KEETPC\ktske <ktskeet@gmail.com>
2025-08-09 13:37:12 -0700

    Further adjustments to transfer details

--- resources/views/character/transfer.blade.php
+++ resources/views/character/transfer.blade.php
@@ -49,9 +49,11 @@
                     Additionally, a mod will need to approve of the transfer. There may be a wait until the recipient receives the character, even after they have confirmed the transfer.
                 @endif
                 <br>
-                <strong>If this is for a Sale, please include the value they were sold for! If it is a gift just state Gift! But if this is a Trade, please do the Trade <u><a href="https://www.alladons.club/trades/open">HERE</a></u> and not via Transfers!</strong>
                 <br>
-                <a href="https://www.alladons.club/guide">More info found here in our Guide under Transfers and Trades</a>
+                <strong>If this is for a SALE, please include the value they were sold for! If it is a GIFT just state Gift! But if this is a TRADE, please do the Trade <u><a href="https://www.alladons.club/trades/open">HERE</a></u> and not via Transfers!</strong>
+                <br>
+                <br>
+                <u><a href="https://www.alladons.club/guide">More info found here in our Guide under Transfers and Trades</a></u>
             </p>
             @if ($cooldown)
                 <p>
