9168702ca8c68e2932988c568ceaf6f2c1ae0641
dustydabean <162489091+dustydabean@users.noreply.github.com>
2026-03-02 13:00:52 -0800

    Removed small-icon for reward

--- resources/views/home/crafting/_recipe_reward_entry.blade.php
+++ resources/views/home/crafting/_recipe_reward_entry.blade.php
@@ -1,1 +1,1 @@
-{{ $reward['quantity'] }} @if(isset($reward['asset']->image_url))<img class="small-icon" src="{{ $reward['asset']->image_url }}">@endif<span>{!! $reward['asset']->displayName !!}</span>
+{{ $reward['quantity'] }} @if(isset($reward['asset']->image_url))<img class="" src="{{ $reward['asset']->image_url }}">@endif<span>{!! $reward['asset']->displayName !!}</span>
