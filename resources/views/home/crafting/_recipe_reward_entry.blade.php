169b3dca6d267140dbfae5f2bd9c73638cd130b6
dustydabean <162489091+dustydabean@users.noreply.github.com>
2026-03-03 13:46:30 -0800

    Fixed recipe ingredient and reward img sizes

--- resources/views/home/crafting/_recipe_reward_entry.blade.php
+++ resources/views/home/crafting/_recipe_reward_entry.blade.php
@@ -1,1 +1,1 @@
-{{ $reward['quantity'] }} @if(isset($reward['asset']->image_url))<img class="" src="">@endif<span>{!! $reward['asset']->displayName !!}</span>
+{{ $reward['quantity'] }} @if(isset($reward['asset']->image_url))<img class="" style="max-width:20%;" src="{{ $reward['asset']->image_url }}">@endif<span>{!! $reward['asset']->displayName !!}</span>
